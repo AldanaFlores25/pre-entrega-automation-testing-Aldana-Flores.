8732aa7b11ddae9a74d822628ed1711980382a6a
Aldana <floresaldana2028@gmail.com>
2025-12-14 23:36:31 -0300

    modifique el archivo run_behave

--- run_behave.py
+++ run_behave.py
@@ -1,8 +1,20 @@
+# run_behave.py
 import subprocess
+import os
 
 print("Ejecutando tests BDD con Behave…")
-subprocess.run([
+
+# aseguramos carpeta del reporte
+os.makedirs("reports/behave", exist_ok=True)
+
+result = subprocess.run([
     "behave",
     "-f", "html",
     "-o", "reports/behave/behave_report.html"
 ])
+
+if result.returncode != 0:
+    print("❌ Algunos tests BDD fallaron")
+    exit(result.returncode)
+
+print("✅ Tests BDD finalizados correctamente")
